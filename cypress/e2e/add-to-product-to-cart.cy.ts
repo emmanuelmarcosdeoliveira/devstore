25a1b8a711335098103f5656df4c00f51c6c4018
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-11-11 21:46:09 -0300

    refactor: The application tests have been modified.

--- cypress/e2e/add-to-product-to-cart.cy.ts
+++ cypress/e2e/add-to-product-to-cart.cy.ts
@@ -18,7 +18,7 @@ describe('add product to cart', () => {
     cy.contains('Cart(1)').should('exist')
   })
   it('should be able to search for a product and add it to the cart', () => {
-    cy.searchByQuery('moletom')
+    cy.get('input[name="q"]').type('moletom').parent('form').submit()
     cy.get('a[href^="/product"]').first().click()
     cy.location('pathname').should('include', '/product')
     cy.contains('Adicionar ao carrinho').click()
--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,7 +1,7 @@
 describe('search products', () => {
   it('should be able to search for products ', () => {
     cy.visit('/')
-    cy.searchByQuery('moletom')
+    cy.get('input[name="q"]').type('moletom').parent('form').submit()
     cy.location('pathname').should('include', '/search')
     cy.location('search').should('include', 'q=moletom')
     cy.get('a[href^="/product"]', { timeout: 10000 }).should('exist')
